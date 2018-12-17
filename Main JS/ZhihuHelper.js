003e50037dcab81a7b15ea6a331244e3fe694c79
Pengmeng.Wu <pengmeng.wu@genewiz.com.cn>
2018-12-17 16:50:35 -0500

    Add Button

--- Main JS/ZhihuHelper.js
+++ Main JS/ZhihuHelper.js
@@ -8,7 +8,7 @@
 // @grant        none
 // ==/UserScript==
 
-(function() {
+function AddPersonalStyle() {
     'use strict';
     var myStyle = document.createElement("style");
     myStyle.setAttribute("id","myStyle");
@@ -22,5 +22,16 @@
     myStyle.sheet.insertRule('.ContentItem-actions{display:none}',4);
     myStyle.sheet.insertRule('.Sticky div a svg{display:none}',5);
     myStyle.sheet.insertRule('.TopstoryItem--advertCard{display:none}',5);
-    console.log(myStyle.sheet.cssRules);
-})();
+    myStyle.sheet.insertRule('.btnFloatCorner{position:fixed;margin:20px;float:right;top:50px}',6);
+}
+AddPersonalStyle();
+
+function AddControlBtn(){
+    var ctlBtn = document.createElement("button");
+    ctlBtn.setAttribute("id", "mainCtlBtn");
+    ctlBtn.setAttribute("class", "btnFloatCorner");
+    var btnText = document.createTextNode("Origional Site");
+    ctlBtn.appendChild(btnText);
+    document.body.appendChild(ctlBtn);
+}
+AddControlBtn();

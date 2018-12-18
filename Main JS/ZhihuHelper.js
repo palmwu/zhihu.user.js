ba58b3dbe345109af14fc0a6618aa72c2bdc9b58
Pengmeng.Wu <pengmeng.wu@genewiz.com.cn>
2018-12-18 11:02:52 -0500

    Display vote btn; Display show less btn

--- Main JS/ZhihuHelper.js
+++ Main JS/ZhihuHelper.js
@@ -2,7 +2,7 @@
 // @name         ZhihuHelper
 // @namespace    http://tampermonkey.net/
 // @version      0.2
-// @description  A real "helper" amony Zhihu
+// @description  A real "helper" for Zhihu
 // @author       You
 // @match        https://*.zhihu.com/*
 // @grant        none
@@ -11,19 +11,28 @@
 
 function AddPersonalStyle() {
     'use strict';
+    //Expend the read div width
+    $(".Topstory-mainColumn")[0].style.width = "100%"
+    
     var myStyle = document.createElement("style");
     myStyle.setAttribute("id","myStyle");
     myStyle.setAttribute("type","text/css");
     myStyle.appendChild(document.createTextNode(""));
     document.head.appendChild(myStyle);
     myStyle.sheet.insertRule('.GlobalSideBar{display:none}',0);
+    //Hide images
     myStyle.sheet.insertRule('figure{display:none}',1);
     myStyle.sheet.insertRule('.ContentItem-title{font-size:15px;font-weight:400}',2);
     myStyle.sheet.insertRule('.RichContent-cover{display:none}',3);
-    myStyle.sheet.insertRule('.ContentItem-actions{display:none}',4);
+    //Buttons at the article bottom
+    myStyle.sheet.insertRule('.Button--withIcon{display:none}',4);
     myStyle.sheet.insertRule('.Sticky div a svg{display:none}',5);
-    myStyle.sheet.insertRule('.TopstoryItem--advertCard{display:none}',5);
-    myStyle.sheet.insertRule('.btnFloatCorner{position:fixed;margin:20px;float:right;top:50px}',6);
+    myStyle.sheet.insertRule('.TopstoryItem--advertCard{display:none}',6);
+    myStyle.sheet.insertRule('.btnFloatCorner{position:fixed;margin:20px;float:right;top:50px}',7);
+    myStyle.sheet.insertRule('.VoteButton {background: #FFF; color: #000;}',8);
+    myStyle.sheet.insertRule('.TopstoryItem--advertCard{display:none}',9);
+    myStyle.sheet.insertRule('svg{display:none}',10);
+    myStyle.sheet.insertRule('.ContentItem-more{float:right}', 11)
 }
 AddPersonalStyle();
 
@@ -38,9 +47,9 @@ function AddControlBtn(){
 AddControlBtn();
 
 /*******************************Below is functions********************************** */
+//Control the img display
 $("#mainCtlBtn").on("click", function (){
     var styleElement = document.getElementById("myStyle");
-    console.log(styleElement.sheet.cssRules[1].style.display);
     if(styleElement.sheet.cssRules[1].style.display == "none"){
         styleElement.sheet.cssRules[1].style.display = "block";
     }else{
